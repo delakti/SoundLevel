8fc2377d82cadf43ecbe2f85b412d4d86f29e369
delakti <delakti@yahoo.co.uk>
2023-11-06 14:31:54 +0000

    Initial Commit

---
+++ SoundLevel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoundLevel
+//
+//  Created by Adeyemi Adelakun on 06/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoundLevel/SoundLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundLevelApp.swift
+//  SoundLevel
+//
+//  Created by Adeyemi Adelakun on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SoundLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
